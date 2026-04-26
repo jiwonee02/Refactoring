bcd02567926a08b43f1f9d7722325180ca45fd0e
jiwonee02 <nepthun@naver.com>
2026-04-26 21:37:34 +0900

    volumeCreditsFor() 함수 옮기기

--- 1.1 Theatre company/index.js
+++ 1.1 Theatre company/index.js
@@ -12,6 +12,7 @@ function statement(invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
@@ -44,6 +45,14 @@ function statement(invoice, plays) {
 
         return result;
     }
+
+    function volumeCreditsFor(aPerformance) {
+        let volumeCredits = 0;
+        volumeCredits += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === aPerformance.play.type)
+            volumeCredits += Math.floor(aPerformance.audience / 5);
+        return volumeCredits;
+    }
 }
 
 function renderPlainText(data) {
@@ -69,7 +78,7 @@ function renderPlainText(data) {
     function totalVolumeCredits() {
         let result = 0;
         for (let perf of data.performances) {
-            result += volumeCreditsFor(perf);
+            result += perf.volumeCredits;
         }
         return result;
     }
@@ -80,14 +89,6 @@ function renderPlainText(data) {
             currency: "USD",
             minimumFractionDigits: 2,
         }).format(aNumber / 100);
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let volumeCredits = 0;
-        volumeCredits += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type)
-            volumeCredits += Math.floor(aPerformance.audience / 5);
-        return volumeCredits;
     }
 }
 
