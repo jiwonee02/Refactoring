119f755ec41ea3cbdd7952a0b92a8c44e7ad24bf
jiwonee02 <nepthun@naver.com>
2026-04-26 21:20:04 +0900

    고객 정보 및 공연 정보 중간 데이터에 포함

--- 1.1 Theatre company/index.js
+++ 1.1 Theatre company/index.js
@@ -2,13 +2,17 @@ import plays from "./plays.json" with { type: "json" };
 import invoices from "./invoices.json" with { type: "json" };
 
 function statement(invoice, plays) {
-    return renderPlainText(invoice, plays);
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances;
+
+    return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(invoice, plays) {
-    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+function renderPlainText(data, plays) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
 
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
         result += ` ${playFor(perf).name}: 
         ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     }
@@ -19,7 +23,7 @@ function renderPlainText(invoice, plays) {
 
     function totalAmount() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += amountFor(perf);
         }
         return result;
@@ -27,7 +31,7 @@ function renderPlainText(invoice, plays) {
 
     function totalVolumeCredits() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += volumeCreditsFor(perf);
         }
         return result;
