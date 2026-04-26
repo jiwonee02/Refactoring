bfbc5b59d769b00b748a997bd4fdf7110e8cf36e
jiwonee02 <nepthun@naver.com>
2026-04-26 20:26:06 +0900

    totalAmount 변수 제거 및 함수 추출

--- 1.1 Theatre company/index.js
+++ 1.1 Theatre company/index.js
@@ -2,17 +2,14 @@ import plays from "./plays.json" with { type: "json" };
 import invoices from "./invoices.json" with { type: "json" };
 
 function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
     for (let perf of invoice.performances) {
-        // 청구 내역을 출력한다.
         result += ` ${playFor(perf).name}: 
         ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-        totalAmount += amountFor(perf);
     }
 
-    result += `총액: ${usd(totalAmount)}\n`;
+    result += `총액: ${usd(totalAmount())}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}점\n`;
     return result;
 }
@@ -64,11 +61,19 @@ function usd(aNumber) {
 }
 
 function totalVolumeCredits() {
-    let volumeCredits = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
+        result += volumeCreditsFor(perf);
     }
-    return volumeCredits;
+    return result;
+}
+
+function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+        result += amountFor(perf);
+    }
+    return result;
 }
 
 console.log(statement(invoices[0], plays));
