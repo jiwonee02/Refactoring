203d4146928288a0a00e67ca6f9a80eb0ffa12c9
jiwonee02 <nepthun@naver.com>
2026-04-26 21:40:41 +0900

    totalVolumeCredits(), totalAmount() 함수 옮기기

--- 1.1 Theatre company/index.js
+++ 1.1 Theatre company/index.js
@@ -5,6 +5,8 @@ function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
 
     return renderPlainText(statementData);
 
@@ -53,19 +55,6 @@ function statement(invoice, plays) {
             volumeCredits += Math.floor(aPerformance.audience / 5);
         return volumeCredits;
     }
-}
-
-function renderPlainText(data) {
-    let result = `청구 내역 (고객명: ${data.customer})\n`;
-
-    for (let perf of data.performances) {
-        result += ` ${perf.play.name}: 
-        ${usd(perf.amount)} (${perf.audience}석)\n`;
-    }
-
-    result += `총액: ${usd(totalAmount())}\n`;
-    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-    return result;
 
     function totalAmount() {
         let result = 0;
@@ -82,6 +71,19 @@ function renderPlainText(data) {
         }
         return result;
     }
+}
+
+function renderPlainText(data) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
+
+    for (let perf of data.performances) {
+        result += ` ${perf.play.name}: 
+        ${usd(perf.amount)} (${perf.audience}석)\n`;
+    }
+
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+    return result;
 
     function usd(aNumber) {
         return new Intl.NumberFormat("en-US", {
