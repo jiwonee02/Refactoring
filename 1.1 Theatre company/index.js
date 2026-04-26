7b30c8fab6e2acb11c4cb6ef99c39bf0a8aa60ce
jiwonee02 <nepthun@naver.com>
2026-04-26 20:21:19 +0900

    volumeCredits 변수 제거 및 함수 추출

--- 1.1 Theatre company/index.js
+++ 1.1 Theatre company/index.js
@@ -3,27 +3,17 @@ import invoices from "./invoices.json" with { type: "json" };
 
 function statement(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2,
-    }).format;
-    for (let perf of invoice.performances) {
-        // 포인트를 적립한다.
-        volumeCredits += volumeCreditsFor(perf);
-        // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" === playFor(perf).type)
-            volumeCredits += Math.floor(perf.audience / 5);
 
+    for (let perf of invoice.performances) {
         // 청구 내역을 출력한다.
         result += ` ${playFor(perf).name}: 
-        ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+        ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `총액: ${format(totalAmount / 100)}\n`;
-    result += `적립 포인트: ${volumeCredits}점\n`;
+
+    result += `총액: ${usd(totalAmount)}\n`;
+    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
     return result;
 }
 
@@ -62,6 +52,22 @@ function volumeCreditsFor(aPerformance) {
     volumeCredits += Math.max(aPerformance.audience - 30, 0);
     if ("comedy" === playFor(aPerformance).type)
         volumeCredits += Math.floor(aPerformance.audience / 5);
+    return volumeCredits;
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+}
+
+function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
+    }
     return volumeCredits;
 }
 
