74a82404b8d6f98c48d8923be7b8d7a6d53f129c
jiwon.han <jiwon.han@snetsystems.co.kr>
2026-04-27 14:43:37 +0900

    타입 코드를 서브 클래스로 바꾸기

--- 1.1 Theatre company/createStatementData.js
+++ 1.1 Theatre company/createStatementData.js
@@ -1,6 +1,17 @@
 import plays from "./plays.json" with { type: "json" };
 import invoices from "./invoices.json" with { type: "json" };
 
+function createPerformanceCalculator(aPerformance, aPlay) {
+    switch (aPlay.type) {
+        case "tragedy":
+            return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy":
+            return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+    }
+}
+
 class PerformanceCalculator {
     constructor(aPerformance, aPlay) {
         this.performance = aPerformance;
@@ -52,7 +63,7 @@ export default function createStatementData(invoice, plays) {
     return result;
 
     function enrichPerformance(aPerformance) {
-        const calculator = new PerformanceCalculator(
+        const calculator = createPerformanceCalculator(
             aPerformance,
             playFor(aPerformance),
         );
